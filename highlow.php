e28b4134f83e0e8feec2de5faba5c908cff286c5
Steven Quintanilla <steven.quintanilla@gmail.com>
2014-12-11 21:05:30 -0600

    Game works, I'm a FUCKING GENIUS!

--- highlow.php
+++ highlow.php
@@ -3,21 +3,22 @@
 $randNum = rand(0, 100);
 
 //Prompts user to guess the  number 
+do {
 fwrite(STDOUT, 'Guess a number between 1 and 100: ');
 $userNum = fgets(STDIN);
 
 //Guess too low
-if ($userNum < $randNum){
-	fwrite(STDOUT, "HIGHER\n");
-}
+	if ($userNum < $randNum){
+		fwrite(STDOUT, "HIGHER\n");
+	}
 //Guess too high
-elseif ($userNum > $randNum){
-	fwrite(STDOUT, "LOWER\n");
-}
+	elseif ($userNum > $randNum){
+		fwrite(STDOUT, "LOWER\n");
+	}
+
+//Keep going  while guess incorrect	
+}while ($userNum != $randNum);
 //Guess correct
-else {
-	fwrite(STDOUT, "YOU GUESS RIGHT!\n");
-} 
-
-
+fwrite(STDOUT, "YOU'RE A FUCKING GENIUS!\n");
+ 
 ?>
